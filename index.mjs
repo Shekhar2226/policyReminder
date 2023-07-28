fb538bcb9b848e2cce4753ff9dd614f5c33f3513
Shekhar2226 <Shekhar.Sharma@kmgin.com>
2023-07-28 10:27:42 +0530

    First Commit

---
+++ index.mjs
@@ -0,0 +1,110 @@
+import express from 'express';
+import nodemailer from 'nodemailer';
+import fetch from 'node-fetch';
+
+import { promisify } from 'util';
+//import AWS from 'aws-sdk';
+//import S3FS from 's3fs';
+
+
+//const s3fsImpl = new S3FS('cyclic-long-erin-kangaroo-garb-ap-southeast-1');
+
+
+const transporter = nodemailer.createTransport({
+  service: 'hotmail',
+  auth: {
+    user: 'Socotra763@outlook.com',
+    pass: 'Socotra123*',
+  },
+});
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/api/hello', (req, res) => {
+  res.send('Hello, World!');
+});
+
+
+
+app.post('/api/sendEmail', async (req, res) => {
+    try {
+      const { id, transactionId, timestamp, data, type, username } = req.body;
+  
+      const policyNumber = data["policyLocator"]; // policyLocator
+  
+      // Creating Authorization token
+      const response_auth = await fetch('https://api.sandbox.socotra.com/account/authenticate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: 'alice.lee',
+          password: 'socotra',
+          hostName: 'ravendra_socotra-configeditor.co.sandbox.socotra.com',
+        }),
+      });
+  
+      if (!response_auth.ok) {
+        throw new Error('Failed to authenticate with Socotra API');
+      }
+  
+      const js_obj_auth = await response_auth.json();
+      const auth_token = js_obj_auth.authorizationToken;
+  
+      // Fetching policy from policyLocator
+      const response_policy = await fetch(`https://api.sandbox.socotra.com/policy/${policyNumber}`, {
+        method: 'GET',
+        headers: {
+          Authorization: auth_token,
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+  
+      if (!response_policy.ok) {
+        throw new Error('Failed to fetch policy data from Socotra API');
+      }
+  
+      const js_obj_policy = await response_policy.json();
+      const email = js_obj_policy.characteristics[0]?.fieldValues?.email;
+      //const doc = js_obj_policy.documents[0]?.url;
+  
+      //const documentType = js_obj_policy.documents[0]?.displayName;
+  
+      
+      if (!email ) {
+        throw new Error('Invalid policy data: missing email ');
+      }
+  
+      await sendEmail(email, 'Hello Reminder', 'Hii I am here.');
+  
+      res.status(200).json({ message: 'Request received successfully' });
+    } catch (error) {
+      console.error('Error:', error.message);
+      res.status(500).json({ error: 'An error occurred while processing the request' });
+    }
+  });
+  
+  async function sendEmail(to, subject, text) {
+    try {
+      const mailOptions = {
+        from: 'Socotra763@outlook.com',
+        to: to,
+        subject: subject,
+        text: text,
+      };
+  
+      const info = await transporter.sendMail(mailOptions);
+      console.log('Email sent:', info.response);
+    } catch (error) {
+      console.error('Error sending email:', error);
+      throw new Error('Failed to send email');
+    }
+  }
+
+  
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
